77ac18c5036ddde5b39a585fd4c1a2b7b61bc72d
Andrei Pestunov <andrei.pestunov@akvelon.com>
2025-03-04 20:09:49 +0100

    Refactor culture handling: rename module paths for improved clarity and consistency, streamline imports, and enhance organization of culture-related services

--- src/Translator.fs
+++ src/Translator.fs
@@ -6,5 +6,5 @@ open Multilang.Domain
 
 let translate (request: Request) : Async<Result<Response, Error'>> =
     match request.Culture with
-    | English -> "Multilang.Translator.Text.translate" |> NotSupported |> Error |> async.Return
-    | Russian -> "Multilang.Translator.Text.translate" |> NotSupported |> Error |> async.Return
+    | English -> "Multilang.Translator.translate" |> NotSupported |> Error |> async.Return
+    | Russian -> "Multilang.Translator.translate" |> NotSupported |> Error |> async.Return
