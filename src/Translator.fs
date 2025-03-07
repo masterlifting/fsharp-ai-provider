cee369c657fbf54b743cc52ac14f21f518d8104a
Andrei Pestunov <andrei.pestunov@akvelon.com>
2025-03-07 16:35:11 +0100

    Refactor client handling: unify client type definitions across modules, streamline client initialization, and enhance type clarity for improved maintainability

--- src/Translator.fs
+++ src/Translator.fs
@@ -8,3 +8,7 @@ let translate (request: Request) : Async<Result<Response, Error'>> =
     match request.Culture with
     | English -> "Multilang.Translator.translate" |> NotSupported |> Error |> async.Return
     | Russian -> "Multilang.Translator.translate" |> NotSupported |> Error |> async.Return
+    
+
+type Type =
+    | OpenAI of Multilang.OpenAI.Domain.Client
---
+++ src/clients/open_ai/Domain.fs
@@ -0,0 +1,8 @@
+﻿[<AutoOpen>]
+module Multilang.OpenAI.Domain
+
+open System
+open System.Collections.Concurrent
+
+type Client = String
+type ClientFactory = ConcurrentDictionary<string, Client>
