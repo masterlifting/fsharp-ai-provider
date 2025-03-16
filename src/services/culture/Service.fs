6db86d058da1a244301464acd104e0d72e6c4b19
Andrey Pestunov <masterlifting@gmail.com>
2025-03-16 23:26:15 +0100

    Refactor persistence and response handling: update file naming, enhance response item versioning, and streamline storage commands

--- src/services/culture/Service.fs
+++ src/services/culture/Service.fs
@@ -7,21 +7,21 @@ open AIProvider.Services.Domain
 open AIProvider.Services.DataAccess.Culture
 open AIProvider.Services.Dependencies
 
-let tryGetFromStorage (request: Culture.Request) (storage: Response.Storage) =
-    storage |> Response.Query.tryGet request
-
 let translate request ct =
     fun (deps: Culture.Dependencies) ->
         let resultAsync = ResultAsyncBuilder()
 
         resultAsync {
-            let! storageResult = deps.Storage |> tryGetFromStorage request
+            let! storageResult = deps.Storage |> Response.Query.get request
 
             return
                 match storageResult with
                 | None ->
                     match deps.Provider with
-                    | AIProvider.Client.Provider.OpenAI client -> client |> OpenAI.Service.translate request ct
+                    | AIProvider.Client.Provider.OpenAI client ->
+                        client
+                        |> OpenAI.Service.translate request ct
+                        |> ResultAsync.bindAsync (fun r -> deps.Storage |> Response.Command.set request.Culture r)
                 | Some cached ->
                     let translatedItems = cached.Items |> List.filter _.Result.IsSome
                     let untranslatedItems = cached.Items |> List.filter _.Result.IsNone
@@ -37,4 +37,5 @@ let translate request ct =
                         |> ResultAsync.map (fun x ->
                             let items = translatedItems @ x.Items
                             { x with Items = items })
+                        |> ResultAsync.bindAsync (fun r -> deps.Storage |> Response.Command.set request.Culture r)
         }
--- src/services/culture/data_access/Response.fs
+++ src/services/culture/data_access/Response.fs
@@ -11,18 +11,19 @@ type Storage = Storage of Storage.Provider
 
 type StorageType = FileSystem of Persistence.FileSystem.Domain.Connection
 
-type ResponseItemEntity(item: ResponseItem) =
+type ResponseItemEntity(item: ResponseItem, version: int) =
     new() =
         ResponseItemEntity(
             { Id = String.Empty
               Value = String.Empty
-              Result = None }
+              Result = None },
+            0
         )
 
     member val Id = item.Id with get, set
     member val Value = item.Value with get, set
     member val Result = item.Result with get, set
-    member val Version = 0 with get, set
+    member val Version = version with get, set
 
     member this.ToDomain() =
         { Id = this.Id
@@ -34,7 +35,7 @@ type ResponseEntity(culture: Culture, response: Response) =
     new() = ResponseEntity(Culture.createDefault (), { Items = [] })
 
     member val Culture = culture.Code with get, set
-    member val Items = response.Items with get, set
+    member val Items = response.Items |> Seq.map (fun x -> ResponseItemEntity(x, 0)) |> Array.ofSeq with get, set
 
 let private toPersistenceStorage storage =
     storage
@@ -46,25 +47,6 @@ let init storageType =
     | FileSystem connection -> connection |> Storage.Connection.FileSystem |> Storage.init
     |> Result.map Storage
 
-module private Common =
-
-    let create (culture: Culture) (response: Response) (data: ResponseEntity array) =
-        match data |> Array.exists (fun x -> x.Culture = culture.Code) with
-        | true -> $"{culture}" |> AlreadyExists |> Error
-        | false -> data |> Array.append [| ResponseEntity(culture, response) |] |> Ok
-
-    let update (culture: Culture) (response: Response) (data: ResponseEntity array) =
-        match data |> Array.tryFindIndex (fun x -> x.Id = response.Id.Value) with
-        | Some index ->
-            data[index] <- response.ToEntity()
-            Ok data
-        | None -> $"{response.Id}" |> NotFound |> Error
-
-    let delete (culture: Culture) (data: ResponseEntity array) =
-        match data |> Array.tryFindIndex (fun x -> x.Id = culture.Value) with
-        | Some index -> data |> Array.removeAt index |> Ok
-        | None -> $"{culture}" |> NotFound |> Error
-
 module private FileSystem =
     open Persistence.FileSystem
 
@@ -72,7 +54,7 @@ module private FileSystem =
 
     module Query =
 
-        let tryGet (request: Request) client =
+        let get (request: Request) client =
             client
             |> loadData
             |> ResultAsync.map (Seq.tryFind (fun x -> x.Culture = request.Culture.Code))
@@ -80,37 +62,57 @@ module private FileSystem =
                 Option.map (fun x ->
                     request.Items
                     |> Seq.map (fun requestItem ->
-                        match
-                            x.Items
-                            |> Seq.filter (fun responseItem -> responseItem.Version = 0)
-                            |> Seq.tryFind (fun responseItem -> responseItem.Id = requestItem.Id)
-                        with
+                        match x.Items |> Seq.tryFind (fun responseItem -> responseItem.Id = requestItem.Id) with
                         | None ->
                             { Id = requestItem.Id
                               Value = requestItem.Value
                               Result = None }
-                        | Some responseItemEntity -> responseItemEntity.ToDomain())
+                        | Some itemEntity -> itemEntity.ToDomain())
                     |> Seq.toList)
             )
             |> ResultAsync.map (Option.map (fun resultItems -> { Items = resultItems }))
 
     module Command =
-        let save culture response client =
+        let set (culture: Culture) (response: Response) client =
             client
             |> loadData
-            |> ResultAsync.bind (Common.create culture response)
+            |> ResultAsync.map (fun data ->
+                match data |> Seq.tryFindIndex (fun x -> x.Culture = culture.Code) with
+                | None ->
+                    let newResponse = ResponseEntity(culture, response)
+                    data[data.Length] <- newResponse
+                    data
+                | Some index ->
+                    let responseEntity = data[index]
+
+                    let responseItemEntities =
+                        response.Items
+                        |> Seq.collect (fun responseItem ->
+                            match
+                                responseEntity.Items
+                                |> Seq.tryFind (fun responseItemEntity -> responseItemEntity.Id = responseItem.Id)
+                            with
+                            | None -> [ ResponseItemEntity(responseItem, 0) ]
+                            | Some existingItemEntity ->
+                                existingItemEntity.Version <- existingItemEntity.Version + 1
+                                let newItemEntity = ResponseItemEntity(responseItem, 0)
+                                [ existingItemEntity; newItemEntity ])
+                        |> Seq.toArray
+
+                    responseEntity.Items <- responseItemEntities
+                    data[index] <- responseEntity
+                    data)
             |> ResultAsync.bindAsync (fun data -> client |> Command.Json.save data)
             |> ResultAsync.map (fun _ -> response)
 
 module Query =
-
-    let tryGet request storage =
+    let get request storage =
         match storage |> toPersistenceStorage with
-        | Storage.FileSystem client -> client |> FileSystem.Query.tryGet request
+        | Storage.FileSystem client -> client |> FileSystem.Query.get request
         | _ -> $"Storage {storage}" |> NotSupported |> Error |> async.Return
 
 module Command =
-    let save culture response storage =
+    let set culture response storage =
         match storage |> toPersistenceStorage with
-        | Storage.FileSystem client -> client |> FileSystem.Command.save culture response
+        | Storage.FileSystem client -> client |> FileSystem.Command.set culture response
         | _ -> $"Storage {storage}" |> NotSupported |> Error |> async.Return
